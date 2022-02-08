b08c1bb06fa44ccd6ae51013b4b908cda40640a6
RimmyLee <lhr0225@gmail.com>
2022-02-08 18:15:05 +0900

    Change search logic for using pipeline.ai-api

--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView, ListView
 from .models import ArticleData, PoliticData
 from django.db.models import Q
+import requests
 
 # Create your views here.
 class homeView(TemplateView):
@@ -11,12 +12,26 @@ class searchView(ListView):
 
     def get_queryset(self):
         query = self.request.GET.get('q')
-        
-        object_list = PoliticData.objects.filter(
-            Q(분류 = query)
-        )
-        
-        return object_list
+        try:
+            response = requests.get(
+                url = 'https://transformers-pipeline.ai-api.datadriven.kr/',
+                params = {
+                    'model_name': 'popo-classifier',
+                    'gitlab_model_path': 'popo-classifier',
+                    'text': query,
+                    'pipeline_type': 'text-classification',
+                }, 
+            )
+
+            json_list = response.json()
+            
+            object_list = PoliticData.objects.filter(
+                Q(분류 = json_list[0]['label'])
+            )
+            
+            return object_list
+        except requests.exceptions.RequestException:
+            print('HTTP Request failed')
 
 class visualizedView(ListView):
     template_name = 'visualized.html'
